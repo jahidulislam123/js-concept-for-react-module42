3553c258af8366483666285f8554f653150be4a9
unknown <islam15-3879@diu.edu.bd>
2022-03-08 21:17:44 +0600

    added

--- destructuring.js
+++ destructuring.js
@@ -3,7 +3,7 @@
 const number =[43,64];
 // const x =number[0];
 // const y=number[1];
-console.log(x,y);
+// console.log(x,y);
 // eta ager sytstem 
 
 
@@ -11,4 +11,9 @@ console.log(x,y);
 const [x,y]=[42,34];
 console.log(x,y);
 
+function voxify(num1,num2){
+    const nums=[num1,num2];
+    return nums;
+}
+console.log(voxify(90,34));
 
